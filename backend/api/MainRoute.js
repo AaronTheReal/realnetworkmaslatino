dc642983a2d5ad3eba284425530a61d86e608553
AaronTheReal <aaron@realmediagroups.com>
2025-07-26 19:55:25 -0600

    diseño

--- backend/api/MainRoute.js
+++ backend/api/MainRoute.js
@@ -2,11 +2,8 @@ import { fileURLToPath } from 'url';
 import multer from 'multer';
 import path from 'path';
 import MainController from './MainController.js';
-import SpotifyController from './SpotifyController.js';
-import NoticiasController from './NoticiasController.js'
-import UsuariosController from './UsuariosController.js'
+
 import AuthController from './AuthController.js';
-import streamingController from './StreamingController.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,33 +28,7 @@ export default class MainRoute {
     //login
     router.route('/login').post(AuthController.login);
 
-    router.route('/search').get(SpotifyController.apiSearchPodcast);
-    router.route('/podcast').post(SpotifyController.savePodcast);
-    router.route('/podcasts').get(SpotifyController.getAllPodcasts);
-    router.route('/podcastIndividual').post(SpotifyController.getPodcastDespliegue);
-    router.post('/showIndividual',SpotifyController.getShowDespliegue.bind(SpotifyController));
 
-    router.route('/shows').get(SpotifyController.getShowsAndEpisodes);
-
-    //noticias
-    //router.route('/noticiasGet').get(NoticiasController.getAllNoticias);
-    router.route('/noticiasPost').post(NoticiasController.createNoticia);
-    router.route('/getNoticias').get(NoticiasController.getAllNoticias);
-    router.route('/getNoticiasInicio').post(NoticiasController.getNoticiaCategorias);
-
-    router.route('/getNoticiaDespliegue').post(NoticiasController.getNoticiaDespliegue);
-
-    router.route('/registrarUsuario').post(UsuariosController.postNuevoUsuario);
-    router.route('/IdiomaUsuarioInicio').put(UsuariosController.postIdiomaUsuario);
-    router.route('/update-language').put(UsuariosController.UpdateLanguagee);
-    router.route('/add-favorites').put(UsuariosController.addToFavorite);
-    router.route('/remove-favorites').put(UsuariosController.removeFromFavorites);
-    router.route('/check-favorite').post(UsuariosController.checkFavorite);
-    router.route('/get-favorites/:userId').get(UsuariosController.getFavorites);
-    router.route('/get-by-category/:category').get(UsuariosController.getAllByCategory);
-    router.route('/get-user/:id').get(UsuariosController.getUserBack);
-    router.route('/crear-stream').post(streamingController.apiCrearStreams);
-    router.route('/get-stream/:id').get(streamingController.apiGetStreams);
     //Stream
    
     
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/pages/home/home.component';
 import { PressComponent } from '../app/pages/press/press.component';
 import { AboutUsComponent } from '../app/pages/about-us/about-us.component';
+import { AdminPanelComponent } from '../app/pages/admin-panel/admin-panel';
 
 export const routes: Routes = [
 
       { path: '', component: HomeComponent },
       { path: 'press-release', component: PressComponent },
       { path: 'about-us', component: AboutUsComponent },
+      { path: 'admin-panel', component: AdminPanelComponent },
 
 ];
